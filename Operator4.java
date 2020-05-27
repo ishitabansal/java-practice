8a6ede97ed2100261ae3b50cb1cff6a4f71e5915
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-27 21:44:05 +0530

    arithmetic

---
+++ Operator4.java
@@ -0,0 +1,10 @@
+class Operator4{
+public static void main(String[] args){
+int x=40;
+int y=25;
+System.out.println(x+y);
+System.out.println(x-y);
+System.out.println(x*y);
+System.out.println(x/y);
+System.out.println(x%y);
+}}
