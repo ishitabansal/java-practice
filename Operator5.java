c82205d93cd3ef187ed92370291bea826f9c497a
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-29 21:44:59 +0530

    operator5

--- Operator5.java
+++ Operator5.java
@@ -1,5 +1,7 @@
-class Operator5{
-public static void main(String[] args){
+class Operator5
+{
+public static void main(String[] args)
+{
 System.out.println(10<<5);
 System.out.println(10>>3);
 System.out.println(20>>3);
