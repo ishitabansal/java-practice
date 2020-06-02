0235e6e1f3430cf26066a495bc845e4254602d1b
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-02 22:32:20 +0530

    operator6

--- Operator6.java
+++ Operator6.java
@@ -1,11 +1,13 @@
-class Operator6{
-public static void main(String[] args){
-int a=9;
-int b=6;
-int c=20;
-System.out.println(a<b&&a++<c);
-System.out.println(a);
-System.out.println(a<b&a++<c);
-System.out.println(a);
-}
+class Operator6
+{
+  public static void main(String[] args)
+  {
+   int a=9;
+   int b=6;
+   int c=20;
+   System.out.println(a<b&&a++<c);
+   System.out.println(a);
+   System.out.println(a<b&a++<c);
+   System.out.println(a);
+  }
 }
