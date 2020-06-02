21b7786ac866814dc8455936acd9bb526eaf204f
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-02 22:33:13 +0530

    Operator4

--- Operator4.java
+++ Operator4.java
@@ -1,12 +1,13 @@
 class Operator4
 {
-public static void main(String[] args)
-{
-int x=40;
-int y=25;
-System.out.println(x+y);
-System.out.println(x-y);
-System.out.println(x*y);
-System.out.println(x/y);
-System.out.println(x%y);
-}}
+  public static void main(String[] args)
+  {
+   int x=40;
+   int y=25;
+   System.out.println(x+y);
+   System.out.println(x-y);
+   System.out.println(x*y);
+   System.out.println(x/y);
+   System.out.println(x%y);
+  }
+}
