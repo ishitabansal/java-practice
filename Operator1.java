0ec31c18be0ba7d88f2d64a7b7422c0278db312b
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-02 22:33:58 +0530

    Operator1

--- Operator1.java
+++ Operator1.java
@@ -1,9 +1,10 @@
 class Operator1
 {
-public static void main(String[] args)
-{
-int a=5;
-int b=5;
-System.out.println(a++ + ++a);
-System.out.println(b++ + ++b);
-}}
+  public static void main(String[] args)
+  {
+   int a=5;
+   int b=5;
+   System.out.println(a++ + ++a);
+   System.out.println(b++ + ++b);
+  }
+}
