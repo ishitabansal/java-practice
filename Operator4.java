46c7ef9cd5ed1cf7d1a8038198bbc27ca4ed2bac
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-04 21:49:46 +0530

    Operator4

--- Operator4.java
+++ Operator4.java
@@ -2,9 +2,9 @@ class Operator4
 {
   public static void main(String[] args)
   {
-   int x=40;
+   int x=40;                    //declare two variable
    int y=25;
-   System.out.println(x+y);
+   System.out.println(x+y);     //making a calculator with two variables
    System.out.println(x-y);
    System.out.println(x*y);
    System.out.println(x/y);
