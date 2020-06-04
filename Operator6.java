5ded0fcb9c1abc8e89cb84270d2c889a279607aa
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-04 21:55:21 +0530

    Operator6

--- Operator6.java
+++ Operator6.java
@@ -2,10 +2,10 @@ class Operator6
 {
   public static void main(String[] args)
   {
-   int a=9;
+   int a=9;                           //declaring the variables
    int b=6;
    int c=20;
-   System.out.println(a<b&&a++<c);
+   System.out.println(a<b&&a++<c);   //logical and bitwise operator
    System.out.println(a);
    System.out.println(a<b&a++<c);
    System.out.println(a);
