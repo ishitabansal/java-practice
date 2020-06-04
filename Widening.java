7060ed694d1f77c1a7a172cd7b3f73b9d6b05914
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-04 22:08:43 +0530

    Widening

--- Widening.java
+++ Widening.java
@@ -2,7 +2,7 @@ class Widening
 {
   public static void main(String[] args)
   {
-   int a=50;
+   int a=50;           //declare integer and float value
    float f=a;
    System.out.println(a);
    System.out.println(f);
