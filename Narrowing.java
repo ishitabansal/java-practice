d70f60045d13d47498073408968001ab940574b3
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-04 21:47:07 +0530

    Narrowing

--- Narrowing.java
+++ Narrowing.java
@@ -2,9 +2,9 @@ class Narrowing
 {
 public static void main(String[] args)
 {
-float f=20.3f;
+float f=20.3f;      //declare the variables
 int a=(int)f;
-System.out.println(f);
+System.out.println(f);    //print the values
 System.out.println(a);
 }
 }
