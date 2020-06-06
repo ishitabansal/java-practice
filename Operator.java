26f823bd8889b6fbf2d4dcb59c9327c10b6b21bc
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-06 22:18:43 +0530

    after comment

--- Operator.java
+++ Operator.java
@@ -2,7 +2,7 @@ class Operator
 {
   public static void main(String[] args)
   {
-   int x=5;
+   int x=5;                    //arithmetic operators
    System.out.println(x++);
    System.out.println(++x);
    System.out.println(x--);
