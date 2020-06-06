a84da4e155c169921d68623b2c8a5c7fffec81e0
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-06 22:26:09 +0530

    after comment

--- Operator3.java
+++ Operator3.java
@@ -2,9 +2,9 @@ class Operator3
 {
   public static void main(String[] args)
   {
-   int x=20;
+   int x=20;                  //declare variables
    int y=-20;
-   boolean a=true;
+   boolean a=true;           //boolean operators
    boolean b=false;
    System.out.println(~x);
    System.out.println(~y);
