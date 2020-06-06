80f4bac1bdab9e005fef923cc828f5b6844162a8
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-06 22:16:24 +0530

    Lower_type

--- Lower_type.java
+++ Lower_type.java
@@ -1,10 +1,10 @@
 class Lower_type
 {
-public static void main(String[] args)
-{
-byte a=30;
-byte b=30;
-byte c=(byte)(a+b);
-System.out.println(c);
-}
+  public static void main(String[] args)
+  {
+   byte a=30;
+   byte b=30;
+   byte c=(byte)(a+b);
+   System.out.println(c);
+  }
 }
