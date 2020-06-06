c3743a79e4b20fe8f5c12606a1d0067bde6c6508
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-06 22:17:23 +0530

    after comment

--- Lower_type.java
+++ Lower_type.java
@@ -2,9 +2,9 @@ class Lower_type
 {
   public static void main(String[] args)
   {
-   byte a=30;
+   byte a=30;             //declaring two byte variables 
    byte b=30;
-   byte c=(byte)(a+b);
+   byte c=(byte)(a+b);   //lower typing the result
    System.out.println(c);
   }
 }
