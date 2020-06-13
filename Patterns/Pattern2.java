120107d096cac7e45f39fd8eb0db84bc60911400
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-13 23:12:24 +0530

    after comment

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -2,11 +2,11 @@ public class Pattern2
 {
 public static void main(String[] args)
 {
-for(int a=1;a<=5;a++)
+for(int a=1;a<=5;a++)                  //no. of rows
 {
-for(int b=1;b<=5;b++)
+for(int b=1;b<=5;b++)                 //no. of columns
 {
-if(a==1 || a==5 || b==1 || b==5)
+if(a==1 || a==5 || b==1 || b==5)     //wherever we need to print the stars, we write in if statement
 {
 System.out.print("* ");
 }
