2dac4489f15c5eddcc514a1c5c82fff205fd09cb
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-14 21:46:05 +0530

    Update Pattern3.java

--- Patterns/Pattern3.java
+++ Patterns/Pattern3.java
@@ -6,7 +6,7 @@ for(int i=1;i<=5;i++)
 {
 for(int j=1;j<=i;j++)
 {
-if((i+j)%2==0)
+if((i+j)%2==0)                            //observe the position 
 System.out.print("1");
 else
 System.out.print("0");
