ae437152ee2cb2b6739391451a095aac42e293e2
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-15 23:09:15 +0530

    after comment

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,16 +1,16 @@
 public class Pattern1
 {
-public static void main(String[] args)
-{
-for(int i=1;i<=4;i++)                   //no. of rows
-{
-for(int j=1;j<=i;j++)                  //no. of columns
-{
-System.out.print("* ");
-}
-System.out.println(" ");
-}
-}
+  public static void main(String[] args)
+  {
+    for(int i=1;i<=4;i++)                   //no. of rows
+    {
+      for(int j=1;j<=i;j++)                  //no. of columns
+      {
+       System.out.print("* ");
+      }
+      System.out.println(" ");
+    }
+  }
 }
 
 
