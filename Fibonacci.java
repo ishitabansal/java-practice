28da279a3a75098058f0c0a6a217a362cf1332f9
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-15 23:21:19 +0530

    third commit

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,18 +1,18 @@
 public class Fibonacci
 {
-public static void main(String[] args)
-{
-int i,x,y;
-i=0;
-x=1;
-y=1;
-System.out.println("1 1");
-while(i<=50)
-{
-i=x+y;
-System.out.println(i+" ");
-x=y;
-y=i;
-}
-}
+  public static void main(String[] args)
+  {
+   int i,x,y;
+   i=0;
+   x=1;
+   y=1;
+   System.out.println("1 1");
+   while(i<=50)
+   {
+    i=x+y;
+    System.out.println(i+" ");
+    x=y;
+    y=i;
+   }
+  }
 }
