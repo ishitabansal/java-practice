6340bee1d1819c8ca7b74b189bbf2722cd558e02
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-15 23:13:48 +0530

    after comment

--- Patterns/Pattern3.java
+++ Patterns/Pattern3.java
@@ -1,19 +1,19 @@
 public class Pattern3
 {
-public static void main(String[] args)
-{
-for(int i=1;i<=5;i++)                    //no. of rows
-{
-for(int j=1;j<=i;j++)                    //no. of columns
-{
-if((i+j)%2==0)                            //observe the position 
-System.out.print("1");
-else
-System.out.print("0");
-}
-System.out.println(" ");
-}
-}
+  public static void main(String[] args)
+  {
+   for(int i=1;i<=5;i++)                    //no. of rows
+   {
+     for(int j=1;j<=i;j++)                    //no. of columns
+     {
+       if((i+j)%2==0)                            //observe the position 
+        System.out.print("1");
+       else
+        System.out.print("0");
+      }
+    System.out.println(" ");
+   }
+ }
 }
 
 
