06ffa709f989842ba0cf4c85d6b78316ea35722a
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-16 23:16:20 +0530

    after comment

--- Labelled_break.java
+++ Labelled_break.java
@@ -1,17 +1,17 @@
 public class Labelled_break
 {
-public static void main(String[] args)
-{
-navin:
-for(int a=1;a<=10;a++)
-{
-for(int b=1;b<=10;b++)
-{
-if(a==7)
-break navin;                //labelled break
-System.out.print("* ");
-}
-System.out.println("");
-}
-}
+ public static void main(String[] args)
+ {
+  navin:
+  for(int a=1;a<=10;a++)
+  {
+   for(int b=1;b<=10;b++)
+   {
+    if(a==7)
+    break navin;                //labelled break
+    System.out.print("* ");
+   }
+   System.out.println("");
+  }
+ }
 }
