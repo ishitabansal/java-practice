4ce53ef60ccd8d07c94b69172ede80bf31e2935c
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-16 23:13:44 +0530

    third commit

--- If_else.java
+++ If_else.java
@@ -1,15 +1,15 @@
 public class If_else
 {
-public static void main(String[] args)
-{
-int n=8;
-if(n%2==0)
-{
-System.out.println("even no.");
-}
-else
-{
-System.out.println("odd no.");
-}
-}
+ public static void main(String[] args)
+ {
+  int n=8;
+  if(n%2==0)
+  {
+   System.out.println("even no.");
+  }
+  else
+  {
+   System.out.println("odd no.");
+  }
+ }
 }
