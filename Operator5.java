f6ee0cb85513013e7fb6242d9461099364857b37
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-07-01 16:00:44 +0530

    add some comment

--- Operator5.java
+++ Operator5.java
@@ -1,10 +1,10 @@
 class Operator5
 {
-  public static void main(String[] args)
+  public static void main(String[] args)            //define main function
   {
-   System.out.println(10<<5);
-   System.out.println(10>>3);
+   System.out.println(10<<5);   //left shift operator            
+   System.out.println(10>>3);    //right shift operator
    System.out.println(20>>3);
-   System.out.println(10>>2);
+   System.out.println(10>>2);       //right shift operator
   }
 }
